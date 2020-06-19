b33ad84206d7d58e1516fc007ec3c5568e185108
MattVarga <MattVarga@users.noreply.github.com>
2020-06-19 03:28:29 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -6,17 +6,21 @@ int main(void)
     int n;
     int i;
     int j;
+    int x;
     
     do
     n = get_int("please enter a hight between 1 and 8:");
-    while (n<0 && n>9);
+    while (n<0 || n>9);
     
         for (i=0; i<n+1; i++){
-             printf("%*s",n-i,"");
+            for (x = (n - i); x >= 0; x--){
+            printf(" ");
+                
+            }
             for (j=0; j<i; j++)
                 {
-                    printf("%s","#");
+                printf("%s","#");
                 }   
-            printf("\n");
+                printf("\n");
         }
 }
