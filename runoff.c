779b9fb35c8e0de8d5ef49eba544d2c1479a9e15
MattVarga <MattVarga@users.noreply.github.com>
2020-06-25 17:42:20 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(candidates[i].name, name))
+        if (!strcmp(candidates[i].name, name))
         {
             preferences[voter][rank] = i;
             return true;
