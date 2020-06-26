dcf063e8685c478c37675093c455cee78cfb5633
MattVarga <MattVarga@users.noreply.github.com>
2020-06-26 01:45:45 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -8,14 +8,15 @@ int main(int argc, char *argv[])
 //int main(void)
 {
     // open input file
-    FILE *input = fopen("card.raw", "r");
+    FILE *input = fopen(argv[1], "r");
+    
     if (input == NULL)
     {
         printf("unable to open card.raw.\n");
         return 1;
     }
     
-    // creates new buffer of 512 bytes
+    // creates new buffer 
     unsigned char buffer[BUFFER_SIZE];
     
     // creates an integer file_num for file names
@@ -24,21 +25,20 @@ int main(int argc, char *argv[])
     FILE *pic = NULL; 
     
     int new_pic = 0; 
-    
+   
     // loops through memory until complete
     while (fread(buffer, BUFFER_SIZE, 1, input) == 1)
     {
-        // read first 4 bytes to see if it is the start of a jpg signature 
+        // read first 4 bytes to see if it is the start of a jpg
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
         {
             if (new_pic == 1)
             {
-                // found the beginning of a new pic
+                // found the beginning of a new pic and need to close current pic
                 fclose(pic);
             }
             else
             {
-                // jpg found
                 new_pic = 1;
             }
             
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
         
         if (new_pic == 1)
         {
-            // write 512 bytes at a time to file if pic found until the next pic is found
+            // write to file until the next pic is found
             fwrite(&buffer, BUFFER_SIZE, 1, pic);
         }
         
